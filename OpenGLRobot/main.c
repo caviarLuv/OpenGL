caf0da5cf327ba71ba3d374d6d709fa4d050e9d6
Tsz Ting Yu <Ashley@Tszs-MacBook-Pro.local>
2018-05-03 13:21:06 -0700

    Initial Commit

---
+++ OpenGLRobot/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OpenGLRobot
+//
+//  Created by Tsz Ting Yu on 5/3/18.
+//  Copyright © 2018 Tsz Ting Yu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

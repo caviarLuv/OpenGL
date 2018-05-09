5967f1147f6f9d730281ce6c773ebddb07e61fb4
Tsz Ting Yu <Ashley@tszs-mbp>
2018-05-09 12:10:03 -0700

    added a automode (just moving on its own randomly) & manual mode. Maybe add a path search. Obstacle generator needs to improve

--- OpenGLRobot/main.c
+++ OpenGLRobot/main.c
@@ -6,207 +6,6 @@
 //  Copyright © 2018 Tsz Ting Yu. All rights reserved.
 //
 // System Headers
-/*
- 
- 
- //---------------------------------------------------------------------------------------------------------------
- //learnGl.com
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <GLUT/GLUT.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-
-//Variables
-const char* vertexShaderSource =
-"#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-
-//color object/fill in
-const char* fragmentShaderSource =
-"#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\n\0";
-//triangles
-float vertices[] = {
-    -0.5f, -0.5f, 0.0f, // left
-    0.5f, -0.5f, 0.0f, // right
-    0.0f,  0.5f, 0.0f  // top
-};
-
-//Rectangle
-float rectangle[] = {
-    0.5f,  0.5f, 0.0f,  // top right
-    0.5f, -0.5f, 0.0f,  // bottom right
-    -0.5f, -0.5f, 0.0f,  // bottom left
-    -0.5f,  0.5f, 0.0f   // top left
-};
-unsigned int indices[] = {  // note that we start from 0!
-    0, 1, 3,   // first triangle
-    1, 2, 3    // second triangle
-};
-
-//Object
-typedef struct cell{
-    
-}Cell;
-//Prototype
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow*);
-void renderGrid();
-void drawTrangle();
-//--------------------------------------------------------------------------------------------------------------------
-//Function definitions
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height){
-    glViewport(0, 0, width, height);
-}
-
-//process key input
-void processInput(GLFWwindow* window){
-    if(glfwGetKey(window,GLFW_KEY_ESCAPE) == GLFW_PRESS){
-        glfwSetWindowShouldClose(window,1);
-    }
-}
-//rendering
-void renderGrid(){
-
-}
-void drawTriangle(){
-    
-}
-
-//-------------------------------------------------------------------------------------------------------------------------
-int main(int argc, char *argv[]){
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);   //needed for mac... Not sure why
-
-    GLFWwindow* window = glfwCreateWindow(800,600,"Test",NULL,NULL);
-    if(window == NULL){
-        printf("Failed to create window");
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    
-    //load Glad --> change some settings depends on operating system
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        printf("Failed to initialize GLAD");
-        return -1;
-    }
-    
-    
-    //initialize buffer
-    unsigned int VBO,VAO;
-    glGenBuffers(1, &VBO);
-    glGenVertexArrays(1, &VAO);
-    
-    //bind array first then set the buffer
-    glBindVertexArray(VAO);
-    
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(rectangle), rectangle, GL_STATIC_DRAW);
-
-    
-    unsigned int EBO;
-    glGenBuffers(1, &EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0); //unbind VBO later; 0 = cancel the connection between GL_ARRAY_BUFFER and VBO
-    glBindVertexArray(0); //unbind VAO later
-    
-    //initialize shader
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    
-    int  success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if(!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n");
-    }
-    
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if(!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n");
-    }
-    //attaching shader to a shader program
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glGetShaderiv(shaderProgram, GL_LINK_STATUS, &success);
-    if(!success)
-    {
-        glGetShaderInfoLog(shaderProgram, 512, NULL, infoLog);
-        printf("ERROR::SHADER::SHADERPROGRAM::LINKING_FAILED\n");
-    }
-    //we can delete the shading program after we link it to the project/program
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-    
-    // uncomment this call to draw in wireframe polygons.
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    
- 
-
-    while(!glfwWindowShouldClose(window)) {
-        //check input
-        processInput(window);
-        //rendering
-        //1.clearScreen-->clearing screen color buffer
-        glClearColor(1.0f,1.0f,1.0f,1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        
-        //actural drawing
-        
-
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
-        
-        
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-    
-    
-    //de-allocate resources
-    glDeleteVertexArrays(1,&VAO);
-    glDeleteBuffers(1, &VBO);
-    
-    glfwTerminate(); //Needs to terminate window before exit
-    return 0;
-
-}*/
-
 
 //---------------------------------------------------------------------------------------------
 //GLFW 2D
@@ -227,6 +26,10 @@ typedef struct car{
     int y;
 }Car;
 
+typedef struct node{
+    int value;
+    struct node* next;
+}Node;
 
 enum Control{
     Up,
@@ -234,6 +37,12 @@ enum Control{
     Left,
     Right
 };
+
+enum Mode{
+    Manual,
+    Auto
+};
+
 
 //Prototype
 void drawGrid(void);
@@ -244,35 +53,78 @@ void drawCar(Car*);
 Cell** initCell(void);
 void generateRandomObstacle(Cell**);
 void updateCar(Car*, int);
+void updateCarAuto();
 int collisionCheck(Car*,int);
 
-//const Variables
+void delay(unsigned int);
+
+//const Variables-ish
 const int gridSize = 32;
 const int gridNum = 10;
 const int SCREEN_WIDTH = gridSize*gridNum + 100;
 const int SCREEN_HEIGHT = gridSize*gridNum + 100;
 Car* car = NULL;
 Cell** gameMap = NULL;
+int MODE = Manual;
 
 //Function Definition
 static void inputK(GLFWwindow* window, int key, int scancode, int action, int mods){
+    if(key == GLFW_KEY_M && action == GLFW_PRESS){
+        MODE = Manual;
+        printf("\nManual Mode");
+    }
+    if(key == GLFW_KEY_A && action == GLFW_PRESS){
+        MODE = Auto;
+        printf("\nAutonomous Mode");
+    }
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
       glfwSetWindowShouldClose(window, GL_TRUE);
     }
-    if(key == GLFW_KEY_LEFT && action == GLFW_PRESS){
+    if(key == GLFW_KEY_LEFT && action == GLFW_PRESS && MODE == Manual){
         updateCar(car,Left);
     }
-    if(key == GLFW_KEY_RIGHT && action == GLFW_PRESS){
+    if(key == GLFW_KEY_RIGHT && action == GLFW_PRESS && MODE == Manual){
         updateCar(car,Right);
     }
-    if(key == GLFW_KEY_UP && action == GLFW_PRESS){
+    if(key == GLFW_KEY_UP && action == GLFW_PRESS && MODE == Manual){
         updateCar(car,Up);
     }
-    if(key == GLFW_KEY_DOWN && action == GLFW_PRESS){
+    if(key == GLFW_KEY_DOWN && action == GLFW_PRESS && MODE == Manual){
         updateCar(car,Down);
     }
 }
 
+void delay(unsigned int sec){
+    unsigned int retTime = time(0) + sec;   // Get finishing time.
+    while (time(0) < retTime);
+}
+
+void updateCarAuto(){
+    int num;
+        num = (rand()%4);
+        if(num == 0){
+            if(collisionCheck(car, num)){
+                car->y = (car->y) - gridSize;
+            }
+        }
+        if(num == 1){
+            if(collisionCheck(car, num)){
+                car->y = (car->y) + gridSize;
+            }
+        }
+        if(num == 2){
+            if(collisionCheck(car, num)){
+                car->x = (car->x) - gridSize;
+            }
+            
+        }
+        if(num == 3){
+            if(collisionCheck(car, num)){
+                car->x = (car->x) + gridSize;
+            }
+        }
+
+}
 static void error_callback(int error, const char* description){
     fputs(description, stderr);
 }
@@ -280,7 +132,6 @@ static void error_callback(int error, const char* description){
 int collisionCheck(Car* car,  int num){
     int row = ((car->y)-50)/gridSize;
     int col = ((car->x)-50)/gridSize;
-    printf("\n%d, %d",row,col);
     if(num == 0){
         if(row > 0){
             if(gameMap[row-1][col].value == 1){
@@ -295,7 +146,7 @@ int collisionCheck(Car* car,  int num){
         }
     }
     else if(num == 1){
-        if(row < gridNum){
+        if(row < gridNum-1){
             if(gameMap[row+1][col].value == 1){
                 return 0;
             }
@@ -322,7 +173,7 @@ int collisionCheck(Car* car,  int num){
 
     }
     else if(num == 3){
-        if(col < gridNum){
+        if(col < gridNum-1){
             if(gameMap[row][col+1].value == 1){
                 return 0;
             }
@@ -391,13 +242,11 @@ void drawGrid(){
     glEnd();
 }
 
-Cell** initCell(){ //initialize a 10x10 cell
-    Cell** gameMap = (Cell**)malloc(gridNum*sizeof(Cell*));
+void generateRandomObstacle(Cell** gameMap){
     int x = 50;
     int y = 50;
     srand(time(0));
     for(int i = 0; i<gridNum;i++){
-        *(gameMap+i) = (Cell*)calloc(gridNum, sizeof(Cell));
         for(int k = 0; k<gridNum;k++){
             if(rand()%10 < 2){
                 
@@ -415,6 +264,14 @@ Cell** initCell(){ //initialize a 10x10 cell
         y+=gridSize;
     }
     gameMap[0][0].value = 0; //car initial position -> NO obstacle
+}
+
+Cell** initCell(){ //initialize a 10x10 cell
+    Cell** gameMap = (Cell**)malloc(gridNum*sizeof(Cell*));
+    for(int i = 0; i<gridNum;i++){
+        *(gameMap+i) = (Cell*)calloc(gridNum, sizeof(Cell));
+    }
+    generateRandomObstacle(gameMap);
     return gameMap;
 }
 
@@ -438,12 +295,13 @@ void drawObstacle(Cell** gameMap){
 }
 
 
+
 int main(void){
     GLFWwindow* window;
     glfwSetErrorCallback(error_callback);
     if (!glfwInit())
         exit(EXIT_FAILURE);
-    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "MSSIN_no", NULL, NULL);
+    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "OpenGL Final Test", NULL, NULL);
     if (!window){
         glfwTerminate();
         exit(EXIT_FAILURE);
@@ -459,6 +317,12 @@ int main(void){
     car->x = 50;
     car->y = 50;
     
+    printf("\n\n"
+           "\n***********************"
+           "\n*    Instructions     *"
+           "\n* Press M for Manual  *"
+           "\n* Press A for Auto    *"
+           "\n***********************");
     while (!glfwWindowShouldClose(window)){
         float ratio;
         int width, height;
@@ -477,8 +341,15 @@ int main(void){
         
         drawGrid();
         drawObstacle(gameMap);
-        drawCar(car);
         
+        if(MODE == Auto){
+            updateCarAuto();
+            drawCar(car);
+            delay(1);
+        }
+        else{
+            drawCar(car);
+        }
         glfwSwapBuffers(window);
         glfwPollEvents();
         
